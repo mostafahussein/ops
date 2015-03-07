0d107e0a654869c38f1cacfb421874ca284b46a6
Z. Liu <liuzx@knownsec.com>
2015-03-07 18:02:07 +0800

    yastat: add git version check

--- scripts/stat_version.py
+++ scripts/stat_version.py
@@ -18,13 +18,17 @@ def stat_version(**kwargs):
                 versions[k] = open(args).read().strip()
             else:
                 versions[k] = "null"
-        elif t in ("dpkg", "exec", "svn"):
+        elif t in ("dpkg", "exec", "svn", "git"):
             if t == "exec":
                 cmd = args
             elif t == "svn":
                 cmd = ("svnversion", "-c", args)
             elif t == "dpkg":
                 cmd = ("dpkg-query", "-W", "-f=${Version}", args)
+            elif t == "git":
+                cmd = ("git", "--git-dir={0}/.git".format(args),
+                        "--work-tree={0}".format(args),
+                        "rev-parse", "--short", "HEAD")
             ret, out = exec_cmd(cmd)
             versions[k] = out.strip()
         else:
